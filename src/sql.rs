30624e78325f675d9f1639f9c2943a1674455d71
TheComamba <thecomamba@jojoheinze.de>
2023-05-14 22:36:47 +0200

    Storing hashes instead of clear text passwords.

--- src/sql.rs
+++ src/sql.rs
@@ -1,4 +1,18 @@
 use rusqlite::Connection;
+use sha2::{Digest, Sha256};
+
+fn hash_str(str: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(str::trim(str).as_bytes());
+    let hash = hasher.finalize();
+
+    // convert hash to String
+    let mut hash_str = String::new();
+    for byte in hash.iter() {
+        hash_str.push_str(&format!("{:02x}", byte));
+    }
+    hash_str
+}
 
 fn establish_connection() -> Result<Connection, rusqlite::Error> {
     let connection = Connection::open("database.db")?;
@@ -14,21 +28,23 @@ fn establish_connection() -> Result<Connection, rusqlite::Error> {
 }
 
 pub(crate) fn add_user(user: &str, password: &str) -> Result<(), rusqlite::Error> {
+    let hash = hash_str(password);
+    println!("Hash: {}", hash);
     let connection = establish_connection()?;
     connection.execute(
         "INSERT INTO credentials (user, password_hash) VALUES (?1, ?2)",
-        (user, password),
+        (user, hash),
     )?;
     Ok(())
 }
 
 pub(crate) fn check_user(user: &str, password: &str) -> Result<bool, rusqlite::Error> {
     let connection = establish_connection()?;
-    let hash: String = connection.query_row(
+    let stored_hash: String = connection.query_row(
         "SELECT password_hash FROM credentials WHERE user = ?1",
         [user],
         |row| row.get(0),
     )?;
 
-    Ok(hash == password)
+    Ok(stored_hash == hash_str(password))
 }
